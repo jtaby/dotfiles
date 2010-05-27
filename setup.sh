fc775015f7de1b3848adaa3e303dc4a9f98e9118
Majd Taby <majd@Majd-Tabys-MacBook-Pro.local>
2010-05-27 00:06:20 -0700

    buncha new stuff, clean up

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
-git clone git://github.com/joelthelion/autojump.git
+
+echo "Time to change your default shell to zsh!"
+chsh -s /bin/zsh
+
 cd autojump
 chmod +x install.zsh
 ./install.zsh
 cd ..
 
+ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vimrc ~/.vimrc
