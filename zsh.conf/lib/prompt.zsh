c05fbfbd0105cf87de3962d0bd6d7273344145d6
Majd Taby <majd@dev2961.snc6.facebook.com>
2011-11-14 16:02:47 -0800

    updated prompt to not be slow

--- zsh.conf/lib/prompt.zsh
+++ zsh.conf/lib/prompt.zsh
@@ -22,7 +22,8 @@ function prompt_char {
 # get the name of the branch we are on
 function git_prompt_info() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+  #echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
 
 # Checks if working tree is dirty
