55dedd434fa8347d25ade5264450ff6526c396aa
Majd Taby <majd@darkroom.co>
2026-01-06 16:21:21 -0800

    install dependencies in setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 
 DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+# Install dependencies
+brew install starship
+brew install fzf
+
 ln -sf "$DOTFILES_DIR/.zshrc" ~/.zshrc
 ln -sf "$DOTFILES_DIR/.zprofile" ~/.zprofile
 ln -sf "$DOTFILES_DIR/.tmux.conf" ~/.tmux.conf
